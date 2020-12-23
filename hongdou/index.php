ee843bcfad750a412c997a22dd894caf118590ab
avfldh <71847635+avfldh@users.noreply.github.com>
2020-12-23 14:11:57 +0000

    Update index.php

--- hongdou/index.php
+++ hongdou/index.php
@@ -1,7 +1,4 @@
 <?php 
-date_default_timezone_set("Asia/Shanghai"); //默认时区
-define('ENVIRONMENT',false); //应用环境，TRUE 打开报错，FALSE关闭报错
-header('content-type:application/json'); //强制编码
 $links=array(
     "top"=>array(
         array("name"=>"顶部导航",    "urls"=>"http://www.xxx.com" ,),
@@ -13,9 +10,4 @@ $links=array(
     ),
     //自动连接放下边
 );
-$result=array("status"=> 1,"data"=>$links,"msg"=>"获取友链成功");
-$json=json_encode($result);
-echo $json;
-// 写入文件
-//file_put_contents('test.json', $json);
 ?>
